30f875faf4a1f5a4c52b3429b3f75c24355ae10e
Mariana <lmarianarp@gmail.com>
2017-09-10 23:30:55 -0700

    02-robot-name

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,27 @@
+class Robot
+  attr_reader :name
 
+  def initialize
+    
+    letters = Array('A'..'Z')
+    name = ''
+    puts letters.sample
+    2.times do
+      name += letters.sample
+    end
+    number = rand (100..1000)
+    @name = name+number.to_s
+  end
+
+  def reset
+    letters = Array('A'..'Z')
+    name = ''
+    puts letters.sample
+    2.times do
+      name += letters.sample
+    end
+    number = rand (100..1000)
+    @name = name+number.to_s
+    return @name
+  end
+end
